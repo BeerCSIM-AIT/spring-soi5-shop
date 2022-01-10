fc37d03f5c130142f8de83d6a7e5241371930d10
BeerCSIM_AIT <st121032@ait.ac.th>
2022-01-10 15:38:29 +0700

    remove unused import

--- src/main/java/nvc/bcit/soi5shop/controller/ProductController.java
+++ src/main/java/nvc/bcit/soi5shop/controller/ProductController.java
@@ -20,7 +20,6 @@ import nvc.bcit.soi5shop.repository.CategoryRepository;
 import nvc.bcit.soi5shop.service.ProductService;
 
 import javax.validation.Valid;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
